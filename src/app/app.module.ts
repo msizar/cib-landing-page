8db45d0f1a4758fc07819d381d55968973bbfb9b
Themba <tfmzworld@gmail.com>
2020-06-25 15:54:23 +0200

    Add bg styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, routingComponents],
